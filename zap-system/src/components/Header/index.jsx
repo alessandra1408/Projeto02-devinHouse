5de878ee1e0f3d3dc9d2dc9a955b9736b86c4c8b
alessandra1408 <alessandraoitnb@gmail.com>
2021-09-14 16:58:28 -0300

    initial header

---
+++ zap-system/src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Header extends Component{
+    render(){
+        return(
+            <>
+            <header>Aqui vai ser o header</header>
+            <ul>
+                <li>Mensagens</li>
+                <li>Dashboard</li>
+            </ul>
+            </>
+        )
+    }
+}
+
+export default Header
