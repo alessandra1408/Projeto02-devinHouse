a9b90d822a66af1c71e05d813e2f73bb35734c97
alessandra1408 <alessandraoitnb@gmail.com>
2021-09-19 19:13:07 -0300

    create table

--- zap-system/src/pages/messagePage/Form/index.jsx
+++ zap-system/src/pages/messagePage/Form/index.jsx
@@ -10,24 +10,52 @@ class Form extends Component{
                 <div id="Inputs">
                     
                     <div>
-                        <label htmlFor="">xxxxx</label>
+                        <label htmlFor="">Gatilho</label>
                         <input type="text" />
                     </div>
                      
-                    
                     <div>
-                        <label htmlFor="">xxxxx</label>
+                        <label htmlFor="">Canal</label>
                         <input type="text" />
                     </div>
 
                     <div>
-                        <label htmlFor="">xxxxx</label>
+                        <label htmlFor="">Timer</label>
                         <input type="text" />
                     </div>
                     
                 </div>
-                
 
+                <div id="table">
+                    <table width="600" border="1px">
+                            <tr class="bold">
+                                <td width="250" height="30">Gatilho</td>
+                                <td width="113">Canal</td>
+                                <td width="140">Tempo</td>
+                                <td width="250">Ações</td>
+                            </tr>
+                            <tr>
+                                <td height="50">abertura_conta</td>
+                                <td>sms</td>
+                                <td>15:00</td>
+                                <td><button>Ver mensagem</button></td>
+                            </tr>
+                            <tr>
+                                <td height="50">fez_pix</td>
+                                <td>sms</td>
+                                <td>5:00</td>
+                                <td><button>Ver mensagem</button></td>
+                            </tr>
+                            <tr>
+                                <td height="50">whatsapp</td>
+                                <td>sms</td>
+                                <td>73:00</td>
+                                <td><button>Ver mensagem</button></td>
+                            </tr>
+                        </table>
+
+                    </div>
+                
             </>
         )
     }
